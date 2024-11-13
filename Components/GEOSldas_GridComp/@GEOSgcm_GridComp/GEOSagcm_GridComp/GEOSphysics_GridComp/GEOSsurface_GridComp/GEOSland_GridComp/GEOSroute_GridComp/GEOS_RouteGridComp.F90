5a7cf10c9bf238d84fc0294b941973beede9baa6
Yujin Zeng <Yujin.Zeng@noaa.gov>
2024-11-13 13:43:55 -0500

    debugging

--- Components/GEOSldas_GridComp/@GEOSgcm_GridComp/GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOSroute_GridComp/GEOS_RouteGridComp.F90
+++ Components/GEOSldas_GridComp/@GEOSgcm_GridComp/GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOSroute_GridComp/GEOS_RouteGridComp.F90
@@ -390,10 +390,10 @@ contains
     real, pointer :: tile_area(:) => NULL()
     real, pointer :: ptr2(:) => NULL()
 
-    real,allocatable,target :: subarea_global(:,:),subarea(:,:) ! Arrays for sub-area and fractions
-    integer,allocatable,target :: subi_global(:,:),subi(:,:)
-    integer,allocatable,target :: nsub_global(:),nsub(:)
-    real,allocatable,target :: area_cat_global(:),area_cat(:)
+    real,pointer :: subarea_global(:,:),subarea(:,:) ! Arrays for sub-area and fractions
+    integer,pointer :: subi_global(:,:),subi(:,:)
+    integer,pointer :: nsub_global(:),nsub(:)
+    real,pointer :: area_cat_global(:),area_cat(:)
     
     type (T_RROUTE_STATE), pointer         :: route => null()
     type (RROUTE_wrap)                     :: wrap
